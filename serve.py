9b472d9be4efd889e1e430a06f65ce42e24c7e85
MaxMin00 <102911137+MaxMin00@users.noreply.github.com>
2024-01-30 14:43:10 -0600

    Add files via upload

---
+++ serve.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST'])
+def post_protocol():
+    subprocess.Popen(["python3", "stress_cpu.py"]) 
+
+
+@app.route("/", methods=['GET'])
+def get_protocol():
+    return socket.gethostbyname(socket.gethostname())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
